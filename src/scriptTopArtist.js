d26fe0226aaa7f14b85d1bb107f163bef372e11b
Maikon Keslley <maikokeslley@gmail.com>
2023-10-26 13:08:46 -0300

    Configurações de redirect URI

--- src/scriptTopArtist.js
+++ src/scriptTopArtist.js
@@ -58,7 +58,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://maikonkc.github.io/who_is_your_drugs");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
