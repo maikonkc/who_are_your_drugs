acd1e98df95fea0c89fd09d60289686b3316e54c
Maikon Keslley <maikokeslley@gmail.com>
2023-10-26 13:51:52 -0300

    Configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/who_is_your_drugs",
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  }
 })
