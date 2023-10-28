716641b223bef577e36b9edf946b6878eaa8d203
Maikon Keslley <maikokeslley@gmail.com>
2023-10-27 21:50:55 -0300

    Para deploy

--- src/scriptTopArtist.js
+++ src/scriptTopArtist.js
@@ -22,7 +22,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     params.append("client_id", clientId);
     params.append("response_type", "code");
     //params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("redirect_uri", "https://maikonkc.github.io/who_is_your_drugs/callback");
+    params.append("redirect_uri", "https://maikonkc.github.io/who_is_your_drugs/index.html");
     params.append("scope", "user-top-read");//user-top-read
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -60,7 +60,7 @@ export async function getAccessToken(clientId, code) {
     params.append("grant_type", "authorization_code");
     params.append("code", code);
     //params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("redirect_uri", "https://maikonkc.github.io/who_is_your_drugs/callback");
+    params.append("redirect_uri", "https://maikonkc.github.io/who_is_your_drugs/index.html");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
