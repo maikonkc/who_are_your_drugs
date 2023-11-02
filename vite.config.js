a288e096bd6390f20c9fa847530e407c2622ca01
Maikon Keslley <maikokeslley@gmail.com>
2023-11-02 15:25:08 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/who_is_your_drugs",
+  base: "/who_are_your_drugs",
   build: {
     target: 'esnext' //browsers can handle the latest ES features
   }
